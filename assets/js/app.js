8290a8a1239685c6e48d162862897b45c82d06f7
LoreMiranda <lorena.miranda.lagos@gmail.com>
2019-01-19 19:29:29 -0300

    ACTUALIZACION ya se muestra la data, cada personaje con su nombre, sin estilo ni ordenado

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,12 @@
  * Acá va solo la lógica
  * 
  */
+
+//console.log(data)
+//const episodes = EPISODES;
+
+//for (let i = 0, i > data.length; i++);
+//for ( let i = 0; i < data.length; i++){
+  //  console.log(data[i].characterName);
+    //console.log(data[i].episodeDescription);
+   // console.log(data[i].episodeTitle);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-/**
- * 
- * Acá va solo el manejo del DOM
- * 
- */
+const containerRoot = document.getElementById("root");
+
+const data = (DATA.characters);
+
+
+const showData = (data) =>{
+    let result = "";
+
+   data.forEach(element => {
+     //   if (element.characterName !== "") {
+    
+        result = containerRoot.innerHTML += `
+        <div>
+        <div class="card">
+        <div class="box">
+        <div class="img">
+        <img src=${element.characterImageThumb}>
+        </div>
+        <h2>${element.characterName}<br></h2>        
+      </div>
+    </div>`
+}
+    );
+  
+
+return result;
+
+}
+window.onload = showData(data)
